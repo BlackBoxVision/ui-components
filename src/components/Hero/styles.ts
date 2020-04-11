61ee050be965ce1f10a375e085df73b0c6ccd8db
Manuel Tuero <manuel.tuero@blackbox-vision.tech>
2020-04-11 01:32:19 -0300

    chore(hero): add support to clipath

---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core';
+
+const clipPath = {
+  none: 'none',
+  left: 'polygon(0 0, 100% 0, 100% 100%, 0 86%)',
+  center: 'polygon(100% 0, 0 0, 0 86%, 50% 100%, 100% 86%)',
+  right: 'polygon(0 0, 100% 0, 100% 86%, 0 100%)',
+};
+
+export const useStyles = makeStyles((theme) => ({
+  section: {
+    clipPath: (props: any) => clipPath[props.clip],
+    display: 'flex',
+    justifyContent: 'center',
+    background: `linear-gradient(45deg, ${theme.palette.primary.light} 0%, ${theme.palette.primary.dark} 100%)`,
+    height: 'calc(100vh - 16px)',
+    position: 'relative',
+  },
+  title: {
+    color: theme.palette.common.white,
+  },
+  subtitle: {
+    color: theme.palette.common.white,
+  },
+  titlesContainer: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    left: '50%',
+    position: 'absolute',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+}));
--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -2,6 +2,7 @@ export const palette = {
   primary: {
     light: '#B9EEE3',
     main: '#5CD2B9',
+    dark: '#3CCFB0',
   },
   secondary: {
     main: '#112F41',
