6df8675a909e5a09d5a0cff83583781310203bf8
jonatansalas <jonatansalas@live.com.ar>
2020-04-13 15:27:07 -0300

    fix(repo card): mimic current site styles for RepoCard component

--- src/components/RepoCard/styles.ts
+++ src/components/RepoCard/styles.ts
@@ -2,11 +2,12 @@ import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles((theme) => ({
   card: {
+    borderRadius: '0.4rem',
     boxShadow: theme.shadows[2],
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
-    minHeight: 160,
+    minHeight: 150,
     height: '100%',
     marginBottom: theme.spacing(2),
     padding: theme.spacing(2),
@@ -14,11 +15,21 @@ export const useStyles = makeStyles((theme) => ({
       boxShadow: theme.shadows[13],
     },
   },
+  title: {
+    fontSize: '1rem',
+    lineHeight: 1.2,
+    fontWeight: 'bold',
+    marginBottom: '1.25rem',
+  },
+  description: {
+    fontSize: 16,
+  },
   infoContainer: {
     alignItems: 'center',
     display: 'flex',
   },
   info: {
     marginLeft: theme.spacing(),
+    fontSize: 16,
   },
 }));
