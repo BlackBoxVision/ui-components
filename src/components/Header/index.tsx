fe027e4d9deee81fa0d378c83e42d456bec6cc06
Manuel Tuero <manuel.tuero@blackbox-vision.tech>
2020-04-13 16:31:10 -0300

    fix(header): remove background and box shadow on collapse

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,13 @@
-import React, { FC, ReactNode } from 'react';
+import React, {
+  FC,
+  ReactNode,
+  useState,
+  useLayoutEffect,
+  useCallback,
+} from 'react';
 import Headroom from 'react-headroom';
 import { AppBar, Toolbar } from '@material-ui/core';
+
 import { useStyles } from './styles';
 
 export interface HeaderProps {
@@ -8,17 +15,51 @@ export interface HeaderProps {
    * React children components
    */
   children?: ReactNode;
+  /**
+   * Header's height
+   */
+  height?: number;
+  /**
+   * A number used to set the header box shadow intensity
+   */
+  elevation?: number;
 }
 
-export const Header: FC<HeaderProps> = ({ children }: HeaderProps) => {
-  const classes = useStyles();
+export const Header: FC<HeaderProps> = ({
+  children,
+  elevation,
+  height,
+}: HeaderProps) => {
+  const [collapsed, setCollapsed] = useState(true);
+  const classes = useStyles({ collapsed, height });
+
+  const handleScroll = useCallback(() => {
+    if (window.scrollY > 64) {
+      setCollapsed(false);
+    } else {
+      setCollapsed(true);
+    }
+  }, []);
+
+  useLayoutEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
     <Headroom>
-      <AppBar position="static" className={classes.appBar}>
+      <AppBar position="fixed" elevation={elevation} className={classes.appBar}>
         <Toolbar className={classes.toolbar}>{children}</Toolbar>
       </AppBar>
     </Headroom>
   );
+};
+
+Header.defaultProps = {
+  elevation: 0,
+  height: 64,
 };
 
 Header.displayName = 'Header';
